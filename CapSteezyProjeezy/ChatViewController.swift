ba264da053d21c53d0c0064ef98203de4f5b3da4
LisetteAntigua <antigua223@gmail.com>
2021-06-10 11:05:02 -0400

    done

--- CapSteezyProjeezy/ChatViewController.swift
+++ CapSteezyProjeezy/ChatViewController.swift
@@ -14,12 +14,34 @@ class ChatViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        menu = SideMenuNavigationController(rootViewController: UIViewController())
+        menu = SideMenuNavigationController(rootViewController: MenuListController())
         menu?.leftSide = true
         SideMenuManager.default.addPanGestureToPresent(toView: self.view)
             }
 
     @IBAction func didTapMenu(){
         present(menu!, animated: true)
+    }
+
+}
+
+class MenuListController: UITableViewController{
+    var menuItems = ["First", "Second"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
